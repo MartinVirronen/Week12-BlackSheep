c47e6dd342e10429502c4c8bd913631b6556b65a
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:16:44 +0200

    xd

---
+++ RandomQuote/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace RandomQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\opilane\Samples\quotes.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filepath);
+
+            Console.WriteLine(dataFromFile[0]);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, dataFromFile.Length);
+            Console.WriteLine(dataFromFile[randomIndex]);
+        }
+    }
+}
